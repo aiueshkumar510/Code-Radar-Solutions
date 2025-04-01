6a0d96704f49882188f71f0c3dccf364051b165d
Aiueshkumar <aiueshkumar954@gmail.com>
2025-04-01 08:13:59 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -3,8 +3,8 @@ void reverse (int arr[],int n);
 void pointerArr(int arr[],int n);
 int main (){
     int a[]={1,2,3,4,5};
-    reverse(arr,5);
-    printArr(arr,5);
+    reverse(arr,4);
+    printArr(arr,4);
     return 0;
 }
 void printArr(int arr[],int n){
